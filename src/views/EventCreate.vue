80f14ad22b410e5da4cbdbe2c3b2966b9f1f54ea
Samuel Laheux <samuel.laheux@novadiscovery.com>
2020-04-02 19:18:45 +0200

    feat(vuex): State + Getters

--- src/views/EventCreate.vue
+++ src/views/EventCreate.vue
@@ -1,3 +1,23 @@
 <template>
-  <h1>Create Event</h1>
+  <div>
+    <h1>Create an Event, {{ user.name }}, {{ user.id }}</h1>
+    <p>{{ getEventById(2) }}</p>
+    <p>There are {{ catLength }} categories</p>
+    <ul>
+      <li v-for="(category, index) in categories" :key="index">
+        {{ category }}
+      </li>
+    </ul>
+  </div>
 </template>
+
+<script>
+import { mapState, mapGetters } from "vuex";
+
+export default {
+  computed: {
+    ...mapGetters(['catLength', 'getEventById']),
+    ...mapState(["user", "categories"])
+  }
+};
+</script>
