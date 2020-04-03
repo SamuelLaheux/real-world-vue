e25c40065a80a6be855bc52cf021ebd61009c723
Samuel Laheux <samuel.laheux@novadiscovery.com>
2020-04-03 18:43:31 +0200

    feat(vuex): Mutations + Actions (2) - add pagination and vuex on EventList

--- src/store/index.js
+++ src/store/index.js
@@ -30,11 +30,18 @@ export default new Vuex.Store({
       { id: 2, title: '...', organizer: '...' },
       { id: 3, title: '...', organizer: '...' },
       { id: 4, title: '...', organizer: '...' }
-    ]
+    ],
+    eventsTotal: null
   },
   mutations: {
     ADD_EVENT(state, event) {
       state.events.push(event)
+    },
+    SET_EVENTS(state, events) {
+      state.events = events
+    },
+    TOTAL_EVENTS(state, eventsTotal) {
+      state.eventsTotal = eventsTotal
     }
   },
   actions: {
@@ -42,6 +49,16 @@ export default new Vuex.Store({
       return EventService.postEvent(event).then(() => {
         commit('ADD_EVENT', event)
       })
+    },
+    fetchEvents({ commit }, { perPage, page }) {
+      return EventService.getEvents(perPage, page)
+        .then(response => {
+          commit('TOTAL_EVENTS', response.headers["x-total-count"])
+          commit('SET_EVENTS', response.data)
+        })
+        .catch(error => {
+          console.log("There was an error:" + error.response);
+        })
     }
   },
   getters: {
